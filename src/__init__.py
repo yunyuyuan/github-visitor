c47de31405c2eb902a8e124b9910cb31e2f5cb58
Q c <chenqiang@exiao.tech>
2021-10-11 16:06:31 +0800

    1

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template, request, Response
+from threading import Event
+
+import MySQLdb
+from flask import Flask, render_template, request, Response, abort
 from flask_cors import CORS
 from os import listdir
 from os.path import splitext, realpath
+
+from flask_mysqldb import MySQL
+
+import config
 from src.util import num_to_list, default_num_parser, digital_num_parser, list_join
-from re import match
-from src.db import DB
+from re import match, sub
 from requests import get
-from pymysql.converters import escape_str
 
 app = Flask(__name__)
 CORS(app)
@@ -16,7 +21,16 @@ app.add_template_filter(list_join)
 app.add_template_filter(default_num_parser)
 app.add_template_filter(digital_num_parser)
 
+app.config['MYSQL_HOST'] = config.db_host
+app.config['MYSQL_USER'] = config.db_user
+app.config['MYSQL_PASSWORD'] = config.db_pwd
+app.config['MYSQL_PORT'] = config.db_port
+app.config['MYSQL_DB'] = config.db_db
+
 themes_file = list(map(lambda file: splitext(file)[0], listdir('templates/themes')))
+mysql = MySQL(app)
+lock = Event()
+lock.set()
 
 
 @app.route('/', methods=['get'])
@@ -26,75 +40,85 @@ def index():
 
 @app.route('/<user>', methods=['get'])
 def visitor(user):
-
-            params = request.args
-            active = params.get('active') or ''
-            active = active if match('^[0-9a-zA-Z]{6}$', active) else '000000'
-            deactive = params.get('deactive') or ''
-            deactive = deactive if match('^[0-9a-zA-Z]{6}$', deactive) else 'f1f1f1'
-            num = 0
-            if user == '_':
-                num = 9527
+    if not match('^[a-zA-Z\d-]*$', user) and user != '_':
+        return abort(403)
+    if user != '_' and sub('^(.*?)/?$', '\\1', request.headers.get('referer', default='')) not in [f'https://github.com/{user}', config.domain, 'http://127.0.0.1:5000']:
+        return abort(403)
+    params = request.args
+    active = params.get('active') or ''
+    active = active if match('^[0-9a-zA-Z]{6}$', active) else '000000'
+    deactive = params.get('deactive') or ''
+    deactive = deactive if match('^[0-9a-zA-Z]{6}$', deactive) else 'f1f1f1'
+    num = 0
+    if user == '_':
+        num = 9527
+    else:
+        # 用户
+        conn = mysql.connection
+        cursor = mysql.connection.cursor()
+        db_user = user
+        cursor.execute("select visitors from visitors where id=%s", (db_user,))
+        v = cursor.fetchone()
+        old_visitors = None
+        lock_v = lock.wait(10)
+        if lock_v:
+            lock.clear()
+            if v:
+                old_visitors = v[0]
             else:
-                # 用户
-                with DB.connection() as _db:
-                    with _db.cursor() as cursor:
-                        db_user = escape_str(user)
-                        cursor.execute('select visitors from visitors where id='+db_user)
-                        v = cursor.fetchone()
-                        old_visitors = None
-                        if v:
-                            old_visitors = v[0]
-                        else:
-                            result = get(f'https://api.github.com/users/{user}')
-                            if result.status_code == 200:
-                                # 新增数据
-                                cursor.execute(f'insert into visitors (id,visitors,last_view) values ("{db_user}", 0, current_date )')
-                                _db.commit()
-                                old_visitors = 0
-                        if old_visitors is not None:
-                            # 插入数据
-                            cursor.execute(f'update visitors set visitors={old_visitors+1},last_view=current_date where id='+db_user)
-                            _db.commit()
-                            num = old_visitors + 1
-                        cursor.close()
-                        _db.close()
+                result = get(f'https://api.github.com/users/{user}')
+                if result.status_code == 200:
+                    # 新增数据
+                    try:
+                        cursor.execute("insert into visitors (id,visitors,last_view) values (%s, 0, current_date )", (db_user,))
+                        old_visitors = 0
+                    except:
+                        cursor.execute("select visitors from visitors where id=%s", (db_user,))
+                        old_visitors = cursor.fetchone()[0]
+            if old_visitors is not None:
+                # 插入数据
+                num = old_visitors + 1
+                cursor.execute("update visitors set visitors=%s,last_view=current_date where id=%s", (num, db_user))
+            conn.commit()
+            lock.set()
 
-            try:
-                animate_speed = min(1000, abs(int(params.get('speed'))))
-            except:
-                animate_speed = 30
-            try:
-                length = abs(int(params.get('len')))
-            except:
-                length = 8
-            try:
-                size = abs(int(params.get('size')))
-            except:
-                size = 36
-            try:
-                space = abs(int(params.get('space')))
-            except:
-                space = 10
-            try:
-                show_tail = bool(abs(int(params.get('tail'))))
-            except:
-                show_tail = True
-            num_list = num_to_list(num, length)
-            num_string = ''.join(map(lambda x: str(max(0, x)), num_list))
-            resp = render_template('visitor.html',
-                                   num_str=num_string,
-                                   num=num_list,
-                                   show_tail=show_tail,
-                                   valid_len=len(str(num)),
-                                   theme=params.get('theme') or 'default',
-                                   active='#' + active,
-                                   deactive='#' + deactive,
-                                   animate_speed=max(30, animate_speed),
-                                   fake_num=[0, 1, 3, 5, 7, 9],
-                                   size=size,
-                                   space=space,
-                                   ).strip()
-            return Response(response=resp, headers={
-                'Content-Type': 'image/svg+xml'
-            })
+    try:
+        animate_speed = min(1000, abs(int(params.get('speed'))))
+    except:
+        animate_speed = 30
+    try:
+        length = abs(int(params.get('len')))
+    except:
+        length = 8
+    try:
+        size = abs(int(params.get('size')))
+    except:
+        size = 36
+    try:
+        space = abs(int(params.get('space')))
+    except:
+        space = 10
+    try:
+        show_tail = bool(abs(int(params.get('tail'))))
+    except:
+        show_tail = True
+    num_list = num_to_list(num, length)
+    num_string = ''.join(map(lambda x: str(max(0, x)), num_list))
+    resp = render_template('visitor.html',
+                           num_str=num_string,
+                           num=num_list,
+                           show_tail=show_tail,
+                           valid_len=len(str(num)),
+                           theme=params.get('theme') or 'default',
+                           active='#' + active,
+                           deactive='#' + deactive,
+                           animate_speed=max(30, animate_speed),
+                           fake_num=[0, 1, 3, 5, 7, 9],
+                           size=size,
+                           space=space,
+                           ).strip()
+    return Response(response=resp, headers={
+        'Content-Type': 'image/svg+xml',
+        'Cache-Control': "no-cache, no-store, must-revalidate",
+        'Expires': "0"
+    })
